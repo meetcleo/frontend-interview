dd9635ea1b3a2e21603fa55401880391f03c2522
Manoj Sethi <manoj@meetcleo.com>
2021-07-27 13:27:57 +0100

    base components and styles.

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GlobalStyles } from './styles';
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
